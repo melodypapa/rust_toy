0acd1723132fb254dd8fd9c3fda3b8c181901112
melodypapa <melodypapa@outlook.com>
2022-03-20 10:44:13 +0800

    update the main

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
-# rust_toy
+# 1. rust_toy
 
-# Create the first hello world project in folder <hello>
-# Create the project with Cargo
+One project to introduce the Rust Language and share the knowledge.
+
+## 1.1. Create the Project
+
+1. Create the first hello world project in folder <hello>
+2. Create the project with Cargo
 
 ```
 cargo new hello
--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,4 +1,19 @@
 fn main() {
     println!("Hello, world!");
+
+    let string_literal = "Hello world 2";
+    print(string_literal.to_string());
+
+    //let test_string = &"Hello word 3";
+    //print(test_string);
+
     std::process::exit(0);
 }
+
+fn print(input: String){
+    println!("{}", input);
+}
+
+/*fn print(input_string: &str) {
+    println!("{}", input_string);
+}*/
